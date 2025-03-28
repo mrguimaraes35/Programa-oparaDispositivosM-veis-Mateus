8e12602f092830419a7891573d21cf79221e6cc1
mrguimaraes35 <mrguimaraes35@gmail.com>
2025-03-27 21:23:00 -0300

    continuaçao react native

--- 05-fundamentos-react-native-II/App.js
+++ 05-fundamentos-react-native-II/App.js
@@ -1,45 +1,62 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView} from 'react-native';
 import PrimeiroComponente from './Componentes/PrimeiroComponente';
 import JavascriptComponente from './Componentes/JavascriptComponente';
 import Perfil from './Componentes/Perfil';
 import ListaComponente from './Componentes/ListaComponente';
+import Atleta from './Componentes/Atleta';
 
 
 
 export default function App() {
+
+  const listaAtletas = [
+    {
+      nome: "Neymar Jr",
+      idade: 33,
+      numero: 10,
+      imagem: 'https://i.pinimg.com/236x/ba/4b/4d/ba4b4d89ee0dcc15ac908758e0eb737a.jpg'
+    },
+    {
+      nome: "Lebron James",
+      idade: 38,
+      numero: 23,
+      imagem: 'https://i.pinimg.com/236x/7c/48/19/7c481977061c7f68bb5970dae91f1787.jpg'
+    },
+    {
+      nome: "Usain Bolt",
+      idade: 40,
+      numero: 1,
+      imagem: 'https://i.pinimg.com/236x/45/b5/d1/45b5d16bf2a2d67cf519ca206fc8d28d.jpg'
+    },
+  ]
+
+
   return (
-    <View style={styles.container}>
-      <StatusBar style="auto" />
+    <ScrollView>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
 
-
-      <ListaComponente />
-
-      {/* <Perfil
-        nome="Gustavo"
-        idade={22}
-        email="teste@gmail.com"
-        telefone="61981111111"
-      />
-
-      <Perfil
-        nome="João"
-        idade={30}
-        email="teste1@gmail.com"
-        telefone="61981111112"
-      />
-
-      <Perfil
-        nome="Maria"
-        idade={18}
-        email="maria@gmail.com"
-        telefone="619800000001"
-      /> */}
+        {
+          listaAtletas.map(
+            atleta => {
+              return (
+                <Atleta
+                  nome={atleta.nome}
+                  idade={atleta.idade}
+                  numero={atleta.numero}
+                  imagem={atleta.imagem}
+                />
+              )
+            }
+          )
+        }
 
 
 
-    </View>
+      </View>
+    </ScrollView>
   );
 }
 
@@ -47,7 +64,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
   },
 });
