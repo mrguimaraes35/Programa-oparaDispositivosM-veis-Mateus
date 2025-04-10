c6999c0dc4070b48439cc3aeee85a991b069a539
mrguimaraes35 <mrguimaraes35@gmail.com>
2025-04-09 21:07:57 -0300

    08 completo

--- 08-navegacao-abas/screens/HomeScreen.jsx
+++ 08-navegacao-abas/screens/HomeScreen.jsx
@@ -1,36 +1,52 @@
-
-import { StyleSheet, Text, View } from 'react-native'
-import { Text } from 'react-native-paper'
 import React from 'react'
-import { Text, Card, Title, Paragraph } from 'react-native-paper'
+import { ScrollView, StyleSheet, View } from 'react-native'
+import { Card, Paragraph, Text, Title } from 'react-native-paper'
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text variant= 'headlineSmall'  style={{ fontWeight: 'bold' }}>Inicio</Text>
-       
-      <Card>
-        <Card.Content>
-          <Title>Um titulo</Title>
-          <Paragraph>mvvtunvuinvununuivnuinuinununuhuiihnruihntvruivht
-            jvurtjhvurjhvujhtuirbijhuhguhuighbuhughbughbugubhughug
-            jfgvbughbguinbuignbuignbung
-          </Paragraph>
-        </Card.Content>
-        <Card.Cover source={{ uri: ''}}/>
-      </Card>
+
+    <ScrollView>
+      <View style={styles.container}>
+
+        <Text variant='headlineSmall' style={{ fontWeight: 'bold' }}>Início</Text>
+
+        <Card style={{ margin: 10 }}>
+          <Card.Content>
+            <Title>Um título</Title>
+            <Paragraph>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        </Card>
+
+        <Card style={{ margin: 10 }}>
+          <Card.Content>
+            <Title>Um título</Title>
+            <Paragraph>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        </Card>
+
+        <Card style={{ margin: 10 }}>
+          <Card.Content>
+            <Title>Um título</Title>
+            <Paragraph>Lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum</Paragraph>
+          </Card.Content>
+          <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
+        </Card>
 
 
+      </View>
 
-    </View>
+    </ScrollView>
+
+
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: 'fff',
-    flex: 1,
     alignItems: 'center',
-    justifyContent: 'center'
+    paddingTop: 10
   }
 })
